3d86506af092fae68570a332ac7305c6347cd6cd
vilaro <vilmarod1@yahoo.com.ar>
2019-08-27 17:59:41 -0300

    Update Prueba.java
    Se modificó el mensaje

--- src/prueba/Prueba.java
+++ src/prueba/Prueba.java
@@ -1,7 +1,6 @@
 package prueba;
 public class Prueba {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        System.out.println("Hola Mickey");
     }
-    
-}
+ }
